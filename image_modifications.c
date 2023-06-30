4a3a19f48d8501f47f82a7ef237dbd13c541db8b
Junjie <129228556+LocalBinNotFound@users.noreply.github.com>
2023-06-30 14:48:43 -0700

    brightness implemented

--- image_modifications.c
+++ image_modifications.c
@@ -4,11 +4,48 @@
 #include "function_buttons.h"
 #include <math.h>
 
-
 void adjustBrightness(GtkWidget* scale, gpointer imageFile) {
+    PreviewBoxWithImage *previewBoxWithImage = imageFile;
+    GdkPixbuf *originalPixbuf = previewBoxWithImage->originalPixbuf;
+
+    if (originalPixbuf != NULL) {
+        int brightnessValue = 20;
+
+        GdkPixbuf *brightPixbuf = gdk_pixbuf_copy(originalPixbuf);
+
+        int width = gdk_pixbuf_get_width(brightPixbuf);
+        int height = gdk_pixbuf_get_height(brightPixbuf);
+        int channels = gdk_pixbuf_get_n_channels(brightPixbuf);
+        int rowstride = gdk_pixbuf_get_rowstride(brightPixbuf);
+
+        guint8 *startPixel = gdk_pixbuf_get_pixels(brightPixbuf);
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                guint8 *pixel = startPixel + y * rowstride + x * channels;
+
+                for (int c = 0; c < channels; c++) {
+                    int newValue = pixel[c] + brightnessValue;
+                    if (newValue < 0)
+                        newValue = 0;
+                    else if (newValue > 255)
+                        newValue = 255;
+                    pixel[c] = newValue;
+                }
+            }
+        }
+
+        g_object_unref(previewBoxWithImage->originalPixbuf);
+        previewBoxWithImage->originalPixbuf = brightPixbuf;
+
+        updatePreviewBox(previewBoxWithImage);
+        g_message("Image brightness adjusted!");
+    } else {
+        g_message("No image available to adjust brightness!");
+    }
 }
 
-void adjustContrast(GtkWidget* scale, gpointer imageFile) {
+
+void adjustContrast(GtkWidget* button, gpointer imageFile) {
 
 }
 
@@ -26,7 +63,7 @@ void gaussianBlur(GtkWidget* scale, gpointer imageFile) {
         guint8* pixels = gdk_pixbuf_get_pixels(blurredPixbuf);
 
         double sigma = 5.0;  // Adjust the sigma value for desired blurring effect
-        int kernelSize = 25;  // Adjust the kernel size for desired blurring effect
+        int kernelSize = 3;  // Adjust the kernel size for desired blurring effect
 
         double kernel[kernelSize][kernelSize];
         double sum = 0.0;
@@ -69,21 +106,31 @@ void gaussianBlur(GtkWidget* scale, gpointer imageFile) {
 }
 
 void laplacianSharpen(GtkWidget* scale, gpointer imageFile) {
+    double laplacianKernel[3][3] = {
+            {-1, -1, -1},
+            {-1, 9, -1},
+            {-1, -1, -1}
+    };
+
 }
 
 void adjustGrayscale(GtkWidget* scale, gpointer imageFile) {
 }
 
-void rotateByDegree() {
+void rotateByDegree(int degree, gpointer imageFile) {
 }
 
-void mirrorImage() {
+void mirrorImageUpDown() {
+}
+
+void mirrorImageLeftRight() {
 }
 
 void adjustRGB(GtkWidget *rScale, GtkWidget *gScale, GtkWidget *bScale, GtkWidget *previewBox) {
+
 }
 
-void invertColor(GtkWidget *button, gpointer imageFile) {
+void invertColor(GtkWidget *scale, gpointer imageFile) {
     PreviewBoxWithImage *previewBoxWithImage = imageFile;
     GdkPixbuf *originalPixbuf = previewBoxWithImage->originalPixbuf;
 
@@ -95,10 +142,10 @@ void invertColor(GtkWidget *button, gpointer imageFile) {
         int channels = gdk_pixbuf_get_n_channels(invertedPixbuf);
         int rowstride = gdk_pixbuf_get_rowstride(invertedPixbuf);
 
-        guint8 *pixels = gdk_pixbuf_get_pixels(invertedPixbuf);
+        guint8 *startPixel = gdk_pixbuf_get_pixels(invertedPixbuf);
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                guint8 *pixel = pixels + y * rowstride + x * channels;
+                guint8 *pixel = startPixel + y * rowstride + x * channels;
                 pixel[0] = 255 - pixel[0];
                 pixel[1] = 255 - pixel[1];
                 pixel[2] = 255 - pixel[2];
@@ -114,4 +161,8 @@ void invertColor(GtkWidget *button, gpointer imageFile) {
     } else {
         g_message("No image available to invert!");
     }
+}
+
+void adjustTransparency() {
+
 }
